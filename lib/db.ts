fc0996321803e0d74e6b6efd7057ec275f6de509
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-26 18:23:01 +0530

    setup the database in next.js

--- lib/db.ts
+++ lib/db.ts
@@ -7,3 +7,30 @@ if (!MONGODB_URI) {
 }
 
 let cached = global.mongoose;
+
+if(!cached) {
+    cached = global.mongoose = {conn: null, promise: null}
+}
+
+export async function connectDB() {
+    if(cached.conn){
+        return cached.conn;
+    } 
+    
+    if(!cached.promise){
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10
+        }
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null;
+        throw error
+    }
+
+    return cached.conn
+}
