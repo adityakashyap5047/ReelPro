b325d7df0d1936afc660c4b8d4e399e769caf357
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-26 22:23:52 +0530

    setup the imagekit provider

---
+++ app/components/Providers.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ImageKitProvider } from "imagekitio-next";
+
+const urlEndpoint = process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT;
+const publicKey = process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY;
+
+export default function Providers({children}: {children: React.ReactNode}) {
+
+    const authenticator = async () => {
+        try {
+          const response = await fetch("/api/imagekit-auth");
+      
+          if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+          }
+      
+          const data = await response.json();
+          const { signature, expire, token } = data;
+          return { signature, expire, token };
+        } catch (error) {
+          console.log(error);
+          throw new Error(`Imagekit Authentication request failed`);
+        }
+      };
+
+    return (
+        <ImageKitProvider urlEndpoint={urlEndpoint} publicKey={publicKey} authenticator={authenticator}>
+            {children}
+        </ImageKitProvider>
+    );
+}
