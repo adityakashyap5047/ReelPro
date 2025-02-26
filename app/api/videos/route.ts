66c5eca95735de4bef9e389b9eb35a7cba346301
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-27 00:24:51 +0530

    completing the route for posting video

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,6 +1,8 @@
+import { authOptions } from "@/lib/auth";
 import { connectDB } from "@/lib/db";
-import Video from "@/models/Video";
-import { NextResponse } from "next/server";
+import Video, { IVideo } from "@/models/Video";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     try {
@@ -16,6 +18,48 @@ export async function GET() {
     } catch{
         return NextResponse.json(
             {error: "Failed to fetch videos"},
+            {status: 500}
+        )
+    }
+}
+
+export async function POST(request: NextRequest){
+    try {
+        const session = await getServerSession(authOptions)
+        if(!session){
+            return NextResponse.json(
+                {error: "Unauthorized User"},
+                {status: 401}
+            )
+        }
+
+        await connectDB();
+        
+        const body:IVideo = await request.json();
+
+        if (!body.title || !body.description || !body.videoUrl || !body.thumbnailUrl) {
+            return NextResponse.json(
+                {error: "All fields are compulsory"},
+                {status: 400}
+            )
+        }
+
+        const videoData = {
+            ...body,
+            controls: body.controls ?? true,
+            transformation: {
+                height: 1920,
+                width: 1080,
+                quality: body.transformation?.quality ?? 100
+            }
+        }
+
+        const newVideo = await Video.create(videoData)
+
+        return NextResponse.json(newVideo);
+    } catch {
+        return NextResponse.json(
+            {error: "Failed to create videos"},
             {status: 500}
         )
     }
